327a4539323e4db18c324fc53ac7d2c5e45930d3
Treasure Kabareebe <treasurecalm@gmail.com>
2021-12-12 23:50:01 +0300

    Move the sum state to the App component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,27 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {  
+  const [sum, setSum] = useState(0);
+
+  function toAdd(initialSum, newValue){
+		setSum(initialSum + newValue);
+	}
+
+	function toSubtract(initialSum, newValue){
+		setSum(initialSum - newValue);
+	}
 
   return (
     <>
-      <Counter value={1} />
-      <Counter value={3} />
-      <Counter value={5} />
-      <Counter value={8} />
+      <h1>{sum}</h1>
+      <div className='counters'>
+        <Counter sum={sum} value={1} toAdd={() => toAdd(sum, 1)} toSubtract={() => toSubtract(sum, 1)}/>
+        <Counter sum={sum} value={3} toAdd={() => toAdd(sum, 3)} toSubtract={() => toSubtract(sum, 3)}/>
+      </div>
+      <div className='counters'>
+        <Counter sum={sum} value={5} toAdd={() => toAdd(sum, 5)} toSubtract={() => toSubtract(sum, 5)}/>
+        <Counter sum={sum} value={8} toAdd={() => toAdd(sum, 8)} toSubtract={() => toSubtract(sum, 8)}/>
+      </div>
     </>
   );
 }
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,27 +2,18 @@ import React from 'react'
 import { useState } from 'react';
 
 export const Counter = (props) => {	
-	const [value, setValue] = useState(props.value);
-	const [sum, setSum] = useState(0);
-
-	function toAdd(initialSum, newValue){
-		setSum(initialSum + newValue);
-	}
-
-	function toSubtract(initialSum, newValue){
-		setSum(initialSum - newValue);
-	}
+	const [value, setValue] = useState(props.value);	
 
 	return (
 		<div>
-			<div>{sum}</div>
+			<div>{props.sum}</div>
 			<div>
-					<span>
-							<button onClick={() => toAdd(sum, value)}>{'+' + value}</button>
-					</span>
-					<span>
-							<button onClick={() => toSubtract(sum, value)}>{'-' + value}</button>
-					</span>
+				<span>
+					<button onClick={props.toAdd}>{'+' + value}</button>
+				</span>
+				<span>
+					<button onClick={props.toSubtract}>{'-' + value}</button>
+				</span>
 			</div>
 		</div>
 	)
